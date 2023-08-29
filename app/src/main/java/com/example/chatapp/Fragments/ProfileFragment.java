fd084a40c18287008811673368b223e2b0f13818
KevinAddae <kevinaddae94@gmail.com>
2023-08-29 19:17:47 +0100

    Begin adding the upload process for the ProfileFragment

--- app/src/main/java/com/example/chatapp/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/chatapp/Fragments/ProfileFragment.java
@@ -1,7 +1,12 @@
  package com.example.chatapp.Fragments;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.utils.widget.ImageFilterView;
 import androidx.fragment.app.Fragment;
@@ -9,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,6 +23,9 @@ import com.example.chatapp.Adapter.UserAdapter;
 import com.example.chatapp.MainActivity;
 import com.example.chatapp.R;
 import com.example.chatapp.model.User;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
@@ -25,6 +34,10 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import org.w3c.dom.Text;
 
@@ -37,6 +50,10 @@ import java.util.concurrent.Executor;
     TextView username;
     FirebaseUser fUser;
     FirebaseFirestore db;
+    StorageReference storageReference;
+    private static final int IMAGE_REQUEST = 1;
+    private Uri imageUri;
+    private StorageTask uploadTask;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,6 +63,9 @@ import java.util.concurrent.Executor;
 
         image_profile = view.findViewById(R.id.profile_image);
         username = view.findViewById(R.id.username);
+
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+
 
         fUser = FirebaseAuth.getInstance().getCurrentUser();
         db = FirebaseFirestore.getInstance();
@@ -60,7 +80,61 @@ import java.util.concurrent.Executor;
             }
         });
 
+        image_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openImage();
+            }
+        });
+
 
         return view;
     }
-}
+
+     private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMAGE_REQUEST);
+     }
+
+     private String getFileExtension(Uri uri){
+         ContentResolver contentResolver = getContext().getContentResolver();
+         MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+         return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+
+     }
+
+     private void uploadImage(){
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Uploading");
+        pd.show();
+
+        if (imageUri != null){
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    + "." + getFileExtension(imageUri));
+
+            uploadTask = fileReference.getFile(imageUri);
+            uploadTask.continueWith(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()){
+                        throw task.getException();
+                    }
+
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        Uri downloadUri = task.getResult();
+                        String myUri = downloadUri.toString();
+
+                        //At this point only the new uploaded Uri is being added to firebase, refer back to previous code,
+                    }
+                }
+            });
+        }
+     }
+ }
